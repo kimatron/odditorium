72c2fd19146cd9082c5a09cf114db67e5970519e
Kim <kimmykim@gmail.com>
2026-01-26 18:29:50 +0000

    add sample products data to js file

---
+++ js/products-data.js
@@ -0,0 +1,80 @@
+/**
+ * PRODUCTS DATA
+ * Array of all products available in the store
+ */
+
+const allProducts = [
+    // Teeth Category
+    {
+        id: 'wisdom-tooth-earrings',
+        name: 'Wisdom Tooth Drop Earrings',
+        artist: 'Dr. Molar\'s Oddities',
+        price: 45.00,
+        category: 'teeth',
+        badge: 'Fresh',
+        image: 'https://images.unsplash.com/photo-1535632066927-ab7c9ab60908?w=400&h=400&fit=crop'
+    },
+    {
+        id: 'baby-tooth-ring',
+        name: 'Baby Tooth Signet Ring',
+        artist: 'Dr. Molar\'s Oddities',
+        price: 38.00,
+        category: 'teeth',
+        image: 'https://images.unsplash.com/photo-1605100804763-247f67b3557e?w=400&h=400&fit=crop'
+    },
+    
+    // Hair & Fur Category
+    {
+        id: 'cat-whisker-necklace',
+        name: 'Cat Whisker Pendant',
+        artist: 'Feline Finds',
+        price: 32.00,
+        category: 'hair',
+        image: 'https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?w=400&h=400&fit=crop'
+    },
+    {
+        id: 'pet-fur-yarn',
+        name: 'Spun Pet Fur Yarn',
+        artist: 'Feline Finds',
+        price: 24.00,
+        category: 'hair',
+        image: 'https://images.unsplash.com/photo-1586339277861-b0b895343ba5?w=400&h=400&fit=crop'
+    },
+    
+    // Tears Category
+    {
+        id: 'tear-crystal-brooch',
+        name: 'Preserved Tear Brooch',
+        artist: 'Emotional Alchemy',
+        price: 68.00,
+        category: 'tears',
+        badge: 'Popular',
+        image: 'https://images.unsplash.com/photo-1611591437281-460bfbe1220a?w=400&h=400&fit=crop'
+    },
+    {
+        id: 'happy-tears-candle',
+        name: 'Happy Tears Candle',
+        artist: 'Emotional Alchemy',
+        price: 18.00,
+        category: 'tears',
+        image: 'https://images.unsplash.com/photo-1602874801006-2b8b4f7d3a62?w=400&h=400&fit=crop'
+    },
+    
+    // Nail Clippings Category
+    {
+        id: 'nail-mosaic',
+        name: 'Nail Clipping Mosaic',
+        artist: 'The Keratin Collective',
+        price: 125.00,
+        category: 'nails',
+        image: 'https://images.unsplash.com/photo-1561214115-f2f134cc4912?w=400&h=400&fit=crop'
+    },
+    {
+        id: 'toenail-coasters',
+        name: 'Resin Toenail Coasters',
+        artist: 'The Keratin Collective',
+        price: 42.00,
+        category: 'nails',
+        image: 'https://images.unsplash.com/photo-1586874473302-bc6c60e6e641?w=400&h=400&fit=crop'
+    }
+];
